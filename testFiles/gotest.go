cd1a56340e31ed683a40cb692b53920b35582a0d
DFreds <dfreds08@gmail.com>
2016-10-24 19:55:54 -0400

    add go language

---
+++ testFiles/gotest.go
@@ -0,0 +1,14 @@
+func test1()
+  some stuff
+  more stuff
+
+no
+func (t Target) test2()
+  some extras
+  more extras
+  okay
+  test1()
+
+func test3(a args)
+func (t Target) test4(a args)
+  blah blah  blah blah
