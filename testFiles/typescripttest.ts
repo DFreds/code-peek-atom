8fa37d0d8fe3b42891f3ca88ea6bf8e98046d215
Derek Fredrickson <dfreds08@gmail.com>
2017-05-12 19:49:58 -0400

    [Issue-27] Support typescript classes

--- testFiles/typescripttest.ts
+++ testFiles/typescripttest.ts
@@ -8,3 +8,46 @@ let myAdd = function(x: number, y: number): number { return x+y; };
 myAdd(2, 3);
 
 addToZ(2,3);
+
+class Greeter {
+    greeting: string;
+    constructor(message: string) {
+        this.greeting = message;
+    }
+    greet() {
+        return "Hello, " + this.greeting;
+    }
+}
+
+let greeter = new Greeter("world");
+
+interface LabelledValue {
+    label: string;
+}
+
+function printLabel(labelledObj: LabelledValue) {
+    console.log(labelledObj.label);
+}
+
+let myObj = {size: 10, label: "Size 10 Object"};
+printLabel(myObj);
+
+class Animal {
+    name: string;
+    constructor(theName: string) { this.name = theName; }
+    move(distanceInMeters: number = 0) {
+        console.log(`${this.name} moved ${distanceInMeters}m.`);
+    }
+}
+
+class Snake extends Animal {
+    constructor(name: string) { super(name); }
+    move(distanceInMeters = 5) {
+        console.log("Slithering...");
+        super.move(distanceInMeters);
+    }
+}
+
+let sam = new Snake("Sammy the Python");
+
+sam.move();
